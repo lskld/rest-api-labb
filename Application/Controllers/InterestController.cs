f3c592afddf098952ccceb01ad031881d154c7c7
lskld <theo.lillieskold@gmail.com>
2026-01-12 10:32:23 +0100

    Added base to InterestController and simple Get method

--- Application/Controllers/InterestController.cs
+++ Application/Controllers/InterestController.cs
@@ -1,6 +1,28 @@
-﻿namespace rest_api_labb.Application.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using rest_api_labb.Application.Context;
+
+namespace rest_api_labb.Application.Controllers
 {
-    public class InterestController
+    [ApiController]
+    [Route("[controller]")]
+    public class InterestController : ControllerBase
     {
+        private readonly RestApiLabbDbContext _context;
+        public InterestController(RestApiLabbDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetInterestsByPerson(int personId)
+        {
+            var interests = await _context.People
+                .Where(p => p.PersonId == personId)
+                .Select(p => p.Interests)
+                .ToListAsync();
+
+            return Ok(interests);
+        }
     }
 }
